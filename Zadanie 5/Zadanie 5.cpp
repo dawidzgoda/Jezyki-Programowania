045b30f92bd7131807e9875a2bd9676022a8d5f2
Dawid Zgoda <acc0rd.2goda@gmail.com>
2024-03-10 15:48:32 +0100

    Zadanie 5

---
+++ Zadanie 5/Zadanie 5.cpp
@@ -0,0 +1,24 @@
+﻿#include "dane.h"
+int toNum8Bit(int bite[8]){
+    int num = 0;
+    for (int i = 0; i < 8; i++) {
+        num += bite[i] * pow(2,i);
+    }
+    return num;
+
+}
+int main()
+{
+    naglowek();
+    int bite[8];
+    cout << "Wprowadz 8 bitów od najstarszego" << endl;
+    for (int i = 0; i <= 7; i++) {
+        int temp;
+        cin >> temp;
+        temp = temp > 1 ? 1 : temp;
+        temp = temp < 0 ? 0 : temp;
+        bite[i] = temp;
+   }
+    cout << "Twoja liczba: " << toNum8Bit(bite) << endl;
+    stop();
+}
---
+++ Zadanie 5/dane.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+void naglowek() {
+    cout << "Jêzyki programowania Grupa 2CZI Zgoda Dawid" << endl;
+    cout << "Modul 1 Funkcje Zadanie 5" << endl;
+}
+void stop() {
+    system("pause");
+}
