b00501a90af5032b7d7371fc650594f2296ee74d
Dawid Zgoda <acc0rd.2goda@gmail.com>
2024-03-10 14:03:47 +0100

    Następne 2 zadania

---
+++ Zadanie 3/Zadanie 3.cpp
@@ -0,0 +1,17 @@
+﻿
+#include "dane.h"
+unsigned long long fib(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return (fib(n - 2) + fib(n - 1)  );
+}
+int main()
+{
+    naglowek();
+    int x;
+    cout << "Wprowadz liczbe do ciągu" << endl;
+    cin >> x;
+    long int s = fib(x);
+    cout << "Wynik: " << fib(x) << endl;
+    stop();
+}
