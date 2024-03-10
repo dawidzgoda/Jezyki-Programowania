c11f7cdf9fc2fcebd15079d9f0cc321b436c4e37
Dawid Zgoda <acc0rd.2goda@gmail.com>
2024-03-10 15:07:04 +0100

    Zadanie 4

---
+++ Zadanie 4/dane.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+void naglowek() {
+    cout << "Jêzyki programowania Grupa 2CZI Zgoda Dawid" << endl;
+    cout << "Modul 1 Funkcje Zadanie 1" << endl;
+}
+void stop() {
+    system("pause");
+}
+struct dec
+{
+    string number;
+};
